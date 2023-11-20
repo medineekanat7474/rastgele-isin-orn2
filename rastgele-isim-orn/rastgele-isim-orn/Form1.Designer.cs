ac69dfbd94874d8bcab88f20d1d9741ceb882ba1
medineekanat7474 <145452232+medineekanat7474@users.noreply.github.com>
2023-11-20 11:15:42 +0300

    Add files via upload

---
+++ rastgele-isim-orn/rastgele-isim-orn/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace rastgele_isim_orn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAra = new System.Windows.Forms.TextBox();
+            this.btnAra = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtAra
+            // 
+            this.txtAra.Location = new System.Drawing.Point(26, 39);
+            this.txtAra.Name = "txtAra";
+            this.txtAra.Size = new System.Drawing.Size(137, 20);
+            this.txtAra.TabIndex = 0;
+            // 
+            // btnAra
+            // 
+            this.btnAra.Location = new System.Drawing.Point(26, 84);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(137, 41);
+            this.btnAra.TabIndex = 1;
+            this.btnAra.Text = "ARA";
+            this.btnAra.UseVisualStyleBackColor = true;
+            this.btnAra.Click += new System.EventHandler(this.btnAra_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "emir",
+            "kevser",
+            "goknur",
+            "meryem",
+            "serpil",
+            "mehmet",
+            "ahmet",
+            "sinan",
+            "yekta",
+            "enes",
+            "emir",
+            "medine",
+            "melek",
+            "ufuk",
+            "asmin",
+            "efe",
+            "tunahan"});
+            this.listBox1.Location = new System.Drawing.Point(201, 39);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(141, 238);
+            this.listBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnAra);
+            this.Controls.Add(this.txtAra);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAra;
+        private System.Windows.Forms.Button btnAra;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ rastgele-isim-orn/rastgele-isim-orn/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rastgele_isim_orn
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAra_Click(object sender, EventArgs e)
+        {
+            string aranan=txtAra.Text;
+            int sayac = 0;
+
+            for(int i = 0; i < listBox1.Items.Count; i++)
+            {
+                if (aranan == listBox1.Items[i].ToString())
+                {
+                    sayac++;
+                }
+            }
+            if (sayac == 0)
+            {
+                MessageBox.Show("öğrenci bulunamadı");
+            }
+            else
+            {
+                MessageBox.Show(sayac+"adatöğrenci bulundu");
+            }
+        }
+    }
+}
